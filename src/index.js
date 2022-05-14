d45b1598a51d5a2d6654c81fa4cdc4386c4e29ae
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-13 23:30:13 -0700

    solving with slice

--- src/index.js
+++ src/index.js
@@ -54,17 +54,8 @@
 // 21.   How have other people solved this problem?
 
 function isPalindrome(str) {
-  let left = 1;
-  let right = str.length - 1;
-  // base case
-    if (isPalindrome(str[left]) !== isPalindrome(str[right])){
-      return false;
-    } else {
-      left++;
-      right--;
-
-    }
-  }
-  // recursion
-  return isPalindrome(str.slice(1)) ) 
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
 }
